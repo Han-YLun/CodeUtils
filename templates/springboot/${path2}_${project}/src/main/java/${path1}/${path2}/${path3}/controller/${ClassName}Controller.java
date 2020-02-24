48197f3614754bd0c2b243f75a9ba0be8dbe305e
ArvinYL <arvinHan@foxmail.com>
2020-02-24 20:35:55 +0800

    Added 通过Freemarker模版生成Controller

--- templates/springboot/${path2}_${project}/src/main/java/${path1}/${path2}/${path3}/controller/${ClassName}Controller.java
+++ templates/springboot/${path2}_${project}/src/main/java/${path1}/${path2}/${path3}/controller/${ClassName}Controller.java
@@ -1,10 +1,11 @@
-package com.ihrm.${ClassName}.controller;
+<#assign classNameLower = ClassName ? uncap_first>
+package ${pPackage}.controller;
 
 import com.ihrm.common.entity.Result;
 import com.ihrm.common.entity.ResultCode;
 import com.ihrm.common.exception.CommonException;
-import com.ihrm.${ClassName}.service.${ClassName}Service;
-import com.ihrm.domain.${ClassName}.${ClassName};
+import ${pPackage}.service.${ClassName}Service;
+import ${pPackage}.pojo.${ClassName};
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,56 +14,48 @@ import java.util.List;
 //解决跨域问题
 @CrossOrigin
 @RestController
-@RequestMapping(value="/${ClassName}")
+@RequestMapping(value="/${classNameLower}")
 public class ${ClassName}Controller {
 
     @Autowired
-    private ${ClassName}Service ${ClassName}Service;
+    private ${ClassName}Service ${classNameLower}Service;
 
-    //保存用户
+    //保存
     @RequestMapping(value="",method = RequestMethod.POST)
-    public Result save(@RequestBody ${ClassName} ${ClassName})  {
-        //业务操作
-        ${ClassName}Service.add(${ClassName});
+    public Result save(@RequestBody ${ClassName} ${classNameLower})  {
+        ${classNameLower}Service.add(${classNameLower});
         return new Result(ResultCode.SUCCESS);
     }
 
-    //根据id更新用户
-    /**
-     * 1.方法
-     * 2.请求参数
-     * 3.响应
-     */
+    //根据id更新
     @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
-    public Result update(@PathVariable(value="id") String id, @RequestBody ${ClassName} ${ClassName} ) {
+    public Result update(@PathVariable(value="id") String id, @RequestBody ${ClassName} ${classNameLower} ) {
         //业务操作
-        ${ClassName}.setId(id);
-        ${ClassName}Service.update(${ClassName});
+        ${classNameLower}.setId(id);
+        ${classNameLower}Service.update(${classNameLower});
         return new Result(ResultCode.SUCCESS);
     }
 
-    //根据id删除用户
+    //根据id删除
     @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
     public Result delete(@PathVariable(value="id") String id) {
-        ${ClassName}Service.deleteById(id);
+        ${classNameLower}Service.deleteById(id);
         return new Result(ResultCode.SUCCESS);
     }
 
-    //根据id查询用户
+    //根据id查询
     @RequestMapping(value="/{id}",method = RequestMethod.GET)
     public Result findById(@PathVariable(value="id") String id) throws CommonException {
-        //throw new CommonException(ResultCode.UNAUTHORISE);
-        ${ClassName} ${ClassName} = ${ClassName}Service.findById(id);
+        ${ClassName} ${classNameLower} = ${classNameLower}Service.findById(id);
         Result result = new Result(ResultCode.SUCCESS);
-        result.setData(${ClassName});
+        result.setData(${classNameLower});
         return result;
     }
 
-    //查询全部用户列表
+    //查询全部列表
     @RequestMapping(value="",method = RequestMethod.GET)
     public Result findAll() {
-        //int i = 1/0;
-        List<${ClassName}> list = ${ClassName}Service.findAll();
+        List<${ClassName}> list = ${classNameLower}Service.findAll();
         Result result = new Result(ResultCode.SUCCESS);
         result.setData(list);
         return result;
